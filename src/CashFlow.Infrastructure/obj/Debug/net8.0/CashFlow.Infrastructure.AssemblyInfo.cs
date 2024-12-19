90e3e422755466d4dc246fdb1f3593b9054e6251
Milton Soares <eng.milton.soares@gmail.com>
2024-12-19 13:04:35 -0300

    feat: report em excel

--- src/CashFlow.Infrastructure/obj/Debug/net8.0/CashFlow.Infrastructure.AssemblyInfo.cs
+++ src/CashFlow.Infrastructure/obj/Debug/net8.0/CashFlow.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d588a5d04f82a9ff3e143aacfec33439031f7fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b6f85136ce7f6c78e3c090fd74d40df7cbfa2d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
